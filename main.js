3a83c5af95f0541031e93d61f33902b6fce3960b
criptodev <cesarmaximo91@gmail.com>
2018-03-24 19:18:16 -0300

    Test

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-console.log(data1);    
-
+//Call for VueJs  
 var main = new Vue({
     el: '.container',
     data: data1
 });
+
+//Data Manipulation
+
+let areasChange = function (sections){
+    
+}
